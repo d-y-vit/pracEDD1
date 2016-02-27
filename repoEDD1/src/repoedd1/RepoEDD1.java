28082775eb97de178326ab805318b7d95e659430
d_y_vit <d_y_vit@d-y-vit-portatil>
2016-02-27 18:23:09 +0100

    Segona modificació des d'Eclipse

--- repoEDD1/src/repoedd1/RepoEDD1.java
+++ repoEDD1/src/repoedd1/RepoEDD1.java
@@ -12,6 +12,7 @@ public class RepoEDD1 {
     public static void main(String[] args) {
     	System.out.print ("Hello World.");
     	System.out.println ("IDE Eclipse");
+    	System.out.println("Segona modificació des d'Eclipse");
     }
     
 }
