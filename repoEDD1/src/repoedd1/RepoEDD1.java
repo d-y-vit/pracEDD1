cd7379abcaaffda1b6b978b93c3384caac83eb04
d_y_vit <d_y_vit@d-y-vit-portatil>
2016-02-27 18:35:37 +0100

    Primera modificació des de Netbeans. Realitzada després de la segon a Eclipse i sense actualitzar repositori

--- repoEDD1/src/repoedd1/RepoEDD1.java
+++ repoEDD1/src/repoedd1/RepoEDD1.java
@@ -10,7 +10,8 @@ public class RepoEDD1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Primera modificació des de Netbeans. Realitzada"
+                + "després de la segon a Eclipse i sense actualitzar repositori");
     }
     
 }
