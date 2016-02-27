8506bfbe20e0bdbe7065b8b12fd308a20c73f242
d_y_vit <d_y_vit@d-y-vit-portatil>
2016-02-27 18:03:36 +0100

    Creat repoEDD1 amb NetBeans

---
+++ repoEDD1/src/repoedd1/RepoEDD1.java
@@ -0,0 +1,16 @@
+package repoedd1;
+
+/**
+ *
+ * @author d_y_vit
+ */
+public class RepoEDD1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
